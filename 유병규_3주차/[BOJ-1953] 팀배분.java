1743f138f6f2b3082ce7d2fc5e7abdb4a700f29c
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2025-02-10 16:32:12 +0900

    fix: [Baekjoon-1953] 팀배분.java

--- 유병규_3주차/[BOJ-1953] 팀배분.java
+++ 유병규_3주차/[BOJ-1953] 팀배분.java
@@ -2,56 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static int[][] graph;
-    private static int n;
-    private static int[] team;
+	private static int n;
+	private static int[][] graph;
+	private static int[] teams;
+	private static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		n = Integer.parseInt(br.readLine());
+		
+		graph = new int[n+1][n+1];
+		teams = new int[n+1];
+		for(int i=1; i<=n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int repeat = Integer.parseInt(st.nextToken());
+			for(int j=0; j<repeat; j++) {
+				graph[i][Integer.parseInt(st.nextToken())] = 1;
+			}
+		}
+		
+		for(int i=1; i<=n; i++) {
+			if(teams[i] == 0) {
+				dfs(i, 1);
+			}
+		}
+		
+		printTeam(1);
+		printTeam(-1);
+		System.out.println(sb);
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+	private static void printTeam(int team) {
+		int count = 0;
+		for(int i=1; i<=n; i++) {
+			if(teams[i] != team) continue;
+			count++;
+		}
+		sb.append(count).append("\n");
+		for(int i=1; i<=n; i++) {
+			if(teams[i] != team) continue;
+			sb.append(i).append(" ");
+		}
+		sb.append("\n");
+	}
 
-        graph = new int[n+1][n+1];
-        for(int i=1; i<=n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int count = Integer.parseInt(st.nextToken());
-            for(int j=1; j<=count; j++) {
-                graph[i][Integer.parseInt(st.nextToken())] = 1;
-            }
-        }
-
-        team = new int[n+1];
-        team[1] = 1;
-        dfs(1, 1);
-
-        int count = 0;
-        StringBuilder team1 = new StringBuilder();
-        StringBuilder team2 = new StringBuilder();
-        for(int i=1; i<=n; i++) {
-            if(team[i] == 1) {
-                count++;
-                team1.append(i+" ");
-                continue;
-            }
-            team2.append(i+" ");
-        }
-
-        System.out.println(count+"\n"+team1.toString().trim()+"\n"
-                +(n-count)+"\n"+team2.toString().trim());
-    }
-
-    private static void dfs(int index, int teamColor) {
-        if(index > n) {
-            return;
-        }
-        for(int i=1; i<=n; i++) {
-            if(graph[index][i] == 0) continue;
-            if(team[i] == teamColor) break;
-
-            team[i] = -teamColor;
-        }
-        if(index+1 > n) return;
-        if(team[index+1] == 0) team[index+1] = teamColor;
-        dfs(index+1, team[index+1]);
-    }
-
+	private static void dfs(int start, int teamColor) {
+		if(teams[start] == 0) {
+			teams[start] = teamColor;
+			for(int i=1; i<=n; i++) {
+				if(graph[start][i] == 0) continue;
+				dfs(i, teamColor*-1);
+			}
+		}
+	}
 }
